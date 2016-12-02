92faabdfe8422ce33b9926ee60c40355484b82da
huangyunfeng <283563375@qq.com>
2016-12-02 08:52:55 +0800

    引入异步redux处理

--- test3/src/main.js
+++ test3/src/main.js
@@ -1,12 +1,20 @@
 import React from 'react' 
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore,applyMiddleware } from 'redux'
 import { Provider,connect } from 'react-redux'
+import thunk from 'redux-thunk'
 import Jisuanqi from './Jisuanqi'
 
 const JIA = {type: 'jia'}
 const JIAN = {type: 'jian'}
 
+let ajaxData = (dispatch,type) => {
+    fetch('https://raw.githubusercontent.com/fengnovo/node-server/master/redux-thunk/a.json')
+        .then(data=>data.json())
+        .then(data=>{
+            dispatch({type:'loadData',payload: data})
+        })
+}
 
 let jisuanqi = (state={list:[]},action) => {
     let list = state.list
@@ -16,13 +24,13 @@ let jisuanqi = (state={list:[]},action) => {
         case 'jian':
             return {list:['a','b','c']}
         case 'loadData':
-            return {list: ['121234233','wqefqw','123fversddv32']}
+            return {list: action.payload}
         default: 
             return state
     }
 }
 
-let store = createStore(jisuanqi);
+let store = createStore(jisuanqi,applyMiddleware(thunk));
 
 let mapStateToProps = () => {
 
@@ -40,9 +48,9 @@ let App = connect(
     },
     dispatch => {
         return {
-            onZengjia: ()=> dispatch(JIA),
-            onJianshao: ()=>dispatch(JIAN),  
-            loadData: (action) => dispatch(action) 
+            onZengjia: () => dispatch(JIA),
+            onJianshao: () => dispatch(JIAN),  
+            loadData: type => ajaxData(dispatch,type)
         }
     }
 )(Jisuanqi)
