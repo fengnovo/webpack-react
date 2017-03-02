f7d6153e833d26a169f4e19ef88038658b854d86
huangyunfeng <283563375@qq.com>
2017-03-02 17:24:52 +0800

    增加理解注释

--- test10/src/containers/FilterLink.js
+++ test10/src/containers/FilterLink.js
@@ -2,9 +2,12 @@ import { connect } from 'react-redux';
 import { setVisibilityFilter } from '../actions';
 import Link from '../components/Link';
 
-const mapStateToProps = (state, ownProps) => ({
-    active: ownProps.filter === state.setVisibilityFilter
-})
+const mapStateToProps = (state, ownProps) => {
+    console.log(ownProps,state); //注意前后state变化
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    }
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
     onClick: () => {
--- test10/src/index.js
+++ test10/src/index.js
@@ -5,7 +5,20 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import reducer from './reducers';
 
-const store = createStore(reducer)
+
+const store = createStore(
+   reducer
+)
+//当开发时chrome浏览器安装了REDUX插件时，可以加下面一句调试
+// const store = createStore(
+//    reducer,
+//    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+
+store.subscribe(() =>{
+  console.log('下面一行打印的是当前整棵树（整个app）的state');
+  console.log(store.getState());
+});
 
 render(
   <Provider store={store}>
